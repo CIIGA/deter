59699ca616958e6197fd7805adf1dde52f8e4c64
CIIGA <ciigasistemas@gmail.com>
2024-02-20 17:09:51 -0600

    20022024-1707

--- acnxerdm/cnx.php
+++ acnxerdm/cnx.php
@@ -1,11 +1,15 @@
 
 <?php
-$serverName = "51.79.98.210";
+
+    session_start();
+
+    $serverName = "51.79.98.210";
     $connectionInfoa = array( 'Database'=>'determinaciones', 'UID'=>'sa', 'PWD'=>'=JeFGm[jFd%J?7j');
     $cnxConect = sqlsrv_connect($serverName, $connectionInfoa);
     date_default_timezone_set('America/Mexico_City');
 
-    $plz=$_GET['idpl']; //cambiar por session
+    //$plz=$_GET['idpl']; //cambiar por session
+    $plz = $_SESSION['m'];
     
     $pro="SELECT * FROM modulos
     inner join proveniente on proveniente.id_proveniente=modulos.id_proveniente
@@ -14,7 +18,7 @@ $serverName = "51.79.98.210";
     $proveniente=sqlsrv_fetch_array($prov);
 
 if(isset($proveniente)){
-    $connectionInfo = array( 'Database'=>$proveniente['data'], 'UID'=>'sa', 'PWD'=>'vrSxHH3TdC');
+    $connectionInfo = array( 'Database'=>$proveniente['data'], 'UID'=>'sa', 'PWD'=>'=JeFGm[jFd%J?7j');
     $cnxa = sqlsrv_connect($serverName, $connectionInfo) or die('No se realizo la conexion ' . print_r(sqlsrv_errors(), true));;
 } else{
     echo 'No se puede establecer conexion';
